3b6706f541f4d2162d152e03380f56c2acbe2e60
Harry Whelchel <harrywhelchel@gmail.com>
2014-05-12 15:11:32 -0400

    Adds readme and removes superfluous Stream methods

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 streams
 =======
+
+I was interested in streams after reading SICP. Influenced by SICP and [stream.js](https://github.com/dionyziz/stream.js)
--- streams.js
+++ streams.js
@@ -24,15 +24,6 @@ Stream.prototype = {
     return this.tail();
   },
 
-  item: function(n) {
-    var stream = this;
-    while ( n !== 0 ) {
-      --n;
-      stream = stream.tail();
-    }
-    return stream.head();
-  },
-
   delay: function(tail) {
     return this._memoTail(tail);
   },
@@ -51,69 +42,10 @@ Stream.prototype = {
     };
   },
 
-  length: function(){
-    var stream = this;
-    var length = 0;
-    while (!stream.empty()){
-      ++length;
-      stream = stream.tail();
-    }
-    return length;
-  },
-
-  add: function(stream){
-    return this.zip(function(x, y){
-      return x + y;
-    }, stream);
-  },
-
-  append: function(stream){
-    if (this.empty()){
-      return stream;
-    }
-    var that = this;
-    return new Stream(
-      that.head(),
-      function(){
-        return that.tail().append(stream);
-      }
-    );
-  },
-
-  zip: function(fn, stream){
-    if ( this.empty() ){
-      return stream;
-    }
-    if ( stream.empty() ){
-      return this;
-    }
-    var that = this;
-    return new Stream( fn(stream.head(), this.head()),
-      function(){
-        return that.tail().zip(fn, stream.tail());
-      });
-  },
-
-  map: function(fn){
-    if (this.empty()) {
-      return this;
-    }
-    var that = this;
-    return new Stream ( fn(this.head()), function(){
-      return that.tail().map(fn);
-    });
-  },
-
   force: function(){
     var stream = this;
     while ( !stream.empty() ){
       stream = stream.tail();
     }
-  },
-
-  scale: function( factor ){
-    return this.map(function(x){
-      return factor * x;
-    });
   }
 };
