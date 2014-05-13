96a9a1033747d935a6be063f718d69b22d557c3d
Harry Whelchel <harrywhelchel@gmail.com>
2014-05-13 12:35:19 -0400

    Refactors stream code

--- streams.js
+++ streams.js
@@ -1,27 +1,27 @@
 // streams
-
 function Stream(head, tail) {
-      if ( typeof head !== 'undefined' ) {
-          this.headValue = head;
-      }
-      if ( typeof tail === 'undefined' ) {
-          tail = function () {
-              return new Stream();
-          };
-      }
-      this.tail = this.delay(tail);
+    if ( typeof head !== 'undefined' ) {
+        this.headValue = head;
+    }
+    if ( typeof tail === 'undefined' ) {
+        tail = function () {
+            return new Stream();
+        };
+    }
+    this.tailValue = this.delay(tail);
   }
 
 Stream.prototype = {
   empty: function() {
-    return typeof this.headValue == 'undefined';
+    return typeof this.head() === 'undefined';
   },
+
   head: function(){
     return this.headValue;
   },
 
   tail: function(){
-    return this.tail();
+    return this.force(this.tailValue);
   },
 
   delay: function(tail) {
@@ -42,10 +42,79 @@ Stream.prototype = {
     };
   },
 
-  force: function(){
-    var stream = this;
-    while ( !stream.empty() ){
-      stream = stream.tail();
+  force: function(delayedTail){
+    return delayedTail();
+  },
+
+  item: function(element) {
+    if (element === 0){
+      return this.head();
+    } else {
+      return this.item.call(this.tail(), element - 1);
+    }
+  },
+
+  map: function(proc){
+    if (this.empty()){
+      return new Stream();
+    } else {
+      return new Stream(proc(this.head()),this.map.call(this.tail(),proc));
+    }
+  },
+
+  each: function(proc){
+    if (this.empty()){
+      return 'done';
+    } else {
+      proc(this.head());
+      this.each.call(this.tail(), proc);
+    }
+  },
+
+  enumerateInterval: function(low, high) {
+    if (low > high) {
+      return new Stream();
+    } else {
+      return new Stream(low, this.enumerateInterval(low + 1, high));
+    }
+  },
+
+  filter: function(predicate){
+    if (this.empty()){
+      return new Stream();
+    } else if (predicate(this.head())) {
+      return new Stream(this.head(),this.filter.call(this.tail(),predicate));
+    } else {
+      return this.filter.call(this.tail(),predicate);
     }
   }
+};
+
+var isPrime = function(num){
+  return num === smallestDivisor(num);
+};
+
+var smallestDivisor = function(num) {
+  var findDivisor = function(num, testDivisor){
+    if (testDivisor*testDivisor > num){
+      return num;
+    } else if (willDivide(testDivisor, num)) {
+      return testDivisor;
+    } else {
+      return findDivisor(num, testDivisor + 1);
+    }
+  };
+  var willDivide = function(a, b){
+    return b % a === 0;
+  };
+  return findDivisor(num, 2);
+};
+
+
+var integersStartingFrom = function(n) {
+  return new Stream(n, integersStartingFrom(n + 1));
+};
+
+var integers = function(){
+  return integersStartingFrom(1);
 };
